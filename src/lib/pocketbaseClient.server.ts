6f85537a561945b8634f3071b174502fbc9b96da
RentonMcIntyre <rentonmcintyre@yahoo.com>
2023-01-02 21:34:47 +0000

    cleaned out data

--- src/lib/pocketbaseClient.server.ts
+++ src/lib/pocketbaseClient.server.ts
@@ -1,10 +1,6 @@
 import PocketBase from 'pocketbase';
-import { PB, PB_USER, PB_PWD } from '$env/static/private'
-
-console.log(PB)
-console.log(PB_USER)
-console.log(PB_PWD)
+import { PB, PB_USER, PB_PWD } from '$env/static/private';
 const PocketBaseClient = new PocketBase(PB);
-PocketBaseClient.admins.authWithPassword('rentonmcintyre@yahoo.com', 'eXq!qdA$4UekAv');
+PocketBaseClient.admins.authWithPassword(PB_USER, PB_PWD);
 
 export { PocketBaseClient }
