c2050a9ea1dac0974daecf9eb16db7745521a402
RentonMcIntyre <rentonmcintyre@yahoo.com>
2023-01-02 21:39:17 +0000

    Banking on await fixing initial server error (#4)
    Pretty sure auth not completing before usage, this should probably be awaited since it returns a promise (we are still just trusting that it will work tho, not going to handle a response)

--- src/lib/pocketbaseClient.server.ts
+++ src/lib/pocketbaseClient.server.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 import { PB, PB_USER, PB_PWD } from '$env/static/private';
 const PocketBaseClient = new PocketBase(PB);
-PocketBaseClient.admins.authWithPassword(PB_USER, PB_PWD);
+await PocketBaseClient.admins.authWithPassword(PB_USER, PB_PWD);
 
 export { PocketBaseClient }
